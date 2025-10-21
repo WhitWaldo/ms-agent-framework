bhargavkagtada/agent-framework

Improve conformance of OpenAI Responses API serving

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Converters/AssistantMessageEventGenerator.cs
@@ -1,0 +1,148 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Common.Id;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Invocation;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Converters;
+
+/// <summary>
+/// A state machine for generating streaming events from assistant message content.
+/// Processes AIContent instances one at a time and emits appropriate streaming events based on internal state.
+/// </summary>
+internal sealed class AssistantMessageEventGenerator(
+        IIdGenerator idGenerator,
+        ISequenceNumber seq,
+        int outputIndex) : StreamingEventGenerator
+{
+    private State _currentState = State.Initial;
+    private readonly string _itemId = idGenerator.GenerateMessageId();
+    private readonly StringBuilder _text = new();
+
+    /// <summary>
+    /// Represents the state of the event generator.
+    /// </summary>
+    private enum State
+    {
+        Initial,
+        AccumulatingText,
+        Completed
+    }
+
+    public override bool IsSupported(AIContent content) => content is TextContent;
+
+    public override IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content)
+    {
+        if (this._currentState == State.Completed)
+        {
+            throw new InvalidOperationException("Cannot process content after the generator has been completed.");
+        }
+
+        // Only process TextContent
+        if (content is not TextContent textContent)
+        {
+            yield break;
+        }
+
+        // If is the first content, emit initial events
+        if (this._currentState == State.Initial)
+        {
+            var incompleteItem = new ResponsesAssistantMessageItemResource
+            {
+                Id = this._itemId,
+                Status = ResponsesMessageItemResourceStatus.InProgress,
+                Content = []
+            };
+
+            yield return new StreamingOutputItemAdded
+            {
+                SequenceNumber = seq.GetNext(),
+                OutputIndex = outputIndex,
+                Item = incompleteItem
+            };
+
+            yield return new StreamingContentPartAdded
+            {
+                SequenceNumber = seq.GetNext(),
+                ItemId = this._itemId,
+                OutputIndex = outputIndex,
+                ContentIndex = 0,
+                Part = new ItemContentOutputText { Text = string.Empty, Annotations = [], Logprobs = [] }
+            };
+
+            this._currentState = State.AccumulatingText;
+        }
+
+        // Accumulate text and emit delta event
+        this._text.Append(textContent.Text);
+
+        yield return new StreamingOutputTextDelta
+        {
+            SequenceNumber = seq.GetNext(),
+            ItemId = this._itemId,
+            OutputIndex = outputIndex,
+            ContentIndex = 0,
+            Delta = textContent.Text
+        };
+    }
+
+    public override IEnumerable<StreamingResponseEvent> Complete()
+    {
+        if (this._currentState == State.Completed)
+        {
+            throw new InvalidOperationException("Complete has already been called.");
+        }
+
+        // If no content was processed, emit initial events first
+        if (this._currentState == State.Initial)
+        {
+            yield break;
+        }
+
+        // Emit final events
+        var finalText = this._text.ToString();
+        var itemContent = new ItemContentOutputText
+        {
+            Text = finalText,
+            Annotations = [],
+            Logprobs = []
+        };
+
+        // Emit response.output_text.done event
+        yield return new StreamingOutputTextDone
+        {
+            SequenceNumber = seq.GetNext(),
+            ItemId = this._itemId,
+            OutputIndex = outputIndex,
+            ContentIndex = 0,
+            Text = finalText
+        };
+
+        yield return new StreamingContentPartDone
+        {
+            SequenceNumber = seq.GetNext(),
+            ItemId = this._itemId,
+            OutputIndex = outputIndex,
+            ContentIndex = 0,
+            Part = itemContent
+        };
+
+        yield return new StreamingOutputItemDone
+        {
+            SequenceNumber = seq.GetNext(),
+            OutputIndex = outputIndex,
+            Item = new ResponsesAssistantMessageItemResource
+            {
+                Id = this._itemId,
+                Status = ResponsesMessageItemResourceStatus.Completed,
+                Content = [itemContent]
+            }
+        };
+
+        this._currentState = State.Completed;
+    }
+}

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Converters/FunctionCallEventGenerator.cs
@@ -1,0 +1,77 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Common.Id;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Invocation;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Converters;
+
+/// <summary>
+/// A generator for streaming events from function call content.
+/// </summary>
+internal sealed class FunctionCallEventGenerator(
+        IIdGenerator idGenerator,
+        ISequenceNumber seq,
+        int outputIndex,
+        JsonSerializerOptions jsonSerializerOptions) : StreamingEventGenerator
+{
+    private bool _isCompleted;
+
+    public override bool IsSupported(AIContent content) => content is FunctionCallContent;
+
+    public override IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content)
+    {
+        if (this._isCompleted)
+        {
+            throw new InvalidOperationException("Cannot process content after the generator has been completed.");
+        }
+
+        if (content is not FunctionCallContent functionCallContent)
+        {
+            throw new InvalidOperationException("FunctionCallEventGenerator only supports FunctionCallContent.");
+        }
+
+        var item = functionCallContent.ToFunctionToolCallItemResource(idGenerator.GenerateFunctionCallId(), jsonSerializerOptions);
+        yield return new StreamingOutputItemAdded
+        {
+            SequenceNumber = seq.GetNext(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        yield return new StreamingFunctionCallArgumentsDelta
+        {
+            SequenceNumber = seq.GetNext(),
+            ItemId = item.Id,
+            OutputIndex = outputIndex,
+            Delta = item.Arguments
+        };
+
+        yield return new StreamingFunctionCallArgumentsDone
+        {
+            SequenceNumber = seq.GetNext(),
+            ItemId = item.Id,
+            OutputIndex = outputIndex,
+            Arguments = item.Arguments
+        };
+
+        yield return new StreamingOutputItemDone
+        {
+            SequenceNumber = seq.GetNext(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        this._isCompleted = true;
+    }
+
+    public override IEnumerable<StreamingResponseEvent> Complete()
+    {
+        this._isCompleted = true;
+        return [];
+    }
+}

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Converters/FunctionResultEventGenerator.cs
@@ -1,0 +1,59 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Common.Id;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Invocation;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Converters;
+
+/// <summary>
+/// A generator for streaming events from function result content.
+/// </summary>
+internal sealed class FunctionResultEventGenerator(
+        IIdGenerator idGenerator,
+        ISequenceNumber seq,
+        int outputIndex) : StreamingEventGenerator
+{
+    private bool _isCompleted;
+
+    public override bool IsSupported(AIContent content) => content is FunctionResultContent;
+
+    public override IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content)
+    {
+        if (this._isCompleted)
+        {
+            throw new InvalidOperationException("Cannot process content after the generator has been completed.");
+        }
+
+        if (content is not FunctionResultContent functionResultContent)
+        {
+            throw new InvalidOperationException("FunctionResultEventGenerator only supports FunctionResultContent.");
+        }
+
+        var item = functionResultContent.ToFunctionToolCallOutputItemResource(idGenerator.GenerateFunctionOutputId());
+        yield return new StreamingOutputItemAdded
+        {
+            SequenceNumber = seq.GetNext(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        yield return new StreamingOutputItemDone
+        {
+            SequenceNumber = seq.GetNext(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        this._isCompleted = true;
+    }
+
+    public override IEnumerable<StreamingResponseEvent> Complete()
+    {
+        this._isCompleted = true;
+        return [];
+    }
+}

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Converters/StreamingEventGenerator.cs
@@ -1,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Converters;
+
+/// <summary>
+/// Abstract base class for generating streaming events from <see cref="AIContent"/> instances
+/// </summary>
+internal abstract class StreamingEventGenerator
+{
+    /// <summary>
+    /// Determines if the provided content is supported by this generator.
+    /// </summary>
+    /// <param name="content">The <see cref="AIContent"/> to check.</param>
+    /// <returns>True if the content is supported, false otherwise.</returns>
+    public abstract bool IsSupported(AIContent content);
+
+    /// <summary>
+    /// Processes a single <see cref="AIContent"/> instance and yields streaming events based on the current state.
+    /// </summary>
+    /// <param name="content">The <see cref="AIContent"/> to process.</param>
+    /// <returns>An enumerable of streaming events generated from processing the content.</returns>
+    public abstract IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content);
+
+    /// <summary>
+    /// Completes the event generation and emits final events.
+    /// </summary>
+    /// <returns>An enumerable of final streaming events.</returns>
+    public abstract IEnumerable<StreamingResponseEvent> Complete();
+}

WhitWaldo/ms-agent-framework

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Streaming/AssistantMessageEventGenerator.cs
@@ -1,0 +1,146 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Streaming;
+
+/// <summary>
+/// A state machine for generating streaming events from assistant message content.
+/// Processes AIContent instances one at a time and emits appropriate streaming events based on internal state.
+/// </summary>
+internal sealed class AssistantMessageEventGenerator(
+        IdGenerator idGenerator,
+        SequenceNumber seq,
+        int outputIndex) : StreamingEventGenerator
+{
+    private State _currentState = State.Initial;
+    private readonly string _itemId = idGenerator.GenerateMessageId();
+    private readonly StringBuilder _text = new();
+
+    /// <summary>
+    /// Represents the state of the event generator.
+    /// </summary>
+    private enum State
+    {
+        Initial,
+        AccumulatingText,
+        Completed
+    }
+
+    public override bool IsSupported(AIContent content) => content is TextContent;
+
+    public override IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content)
+    {
+        if (this._currentState == State.Completed)
+        {
+            throw new InvalidOperationException("Cannot process content after the generator has been completed.");
+        }
+
+        // Only process TextContent
+        if (content is not TextContent textContent)
+        {
+            yield break;
+        }
+
+        // If is the first content, emit initial events
+        if (this._currentState == State.Initial)
+        {
+            var incompleteItem = new ResponsesAssistantMessageItemResource
+            {
+                Id = this._itemId,
+                Status = ResponsesMessageItemResourceStatus.InProgress,
+                Content = []
+            };
+
+            yield return new StreamingOutputItemAdded
+            {
+                SequenceNumber = seq.Increment(),
+                OutputIndex = outputIndex,
+                Item = incompleteItem
+            };
+
+            yield return new StreamingContentPartAdded
+            {
+                SequenceNumber = seq.Increment(),
+                ItemId = this._itemId,
+                OutputIndex = outputIndex,
+                ContentIndex = 0,
+                Part = new ItemContentOutputText { Text = string.Empty, Annotations = [], Logprobs = [] }
+            };
+
+            this._currentState = State.AccumulatingText;
+        }
+
+        // Accumulate text and emit delta event
+        this._text.Append(textContent.Text);
+
+        yield return new StreamingOutputTextDelta
+        {
+            SequenceNumber = seq.Increment(),
+            ItemId = this._itemId,
+            OutputIndex = outputIndex,
+            ContentIndex = 0,
+            Delta = textContent.Text
+        };
+    }
+
+    public override IEnumerable<StreamingResponseEvent> Complete()
+    {
+        if (this._currentState == State.Completed)
+        {
+            throw new InvalidOperationException("Complete has already been called.");
+        }
+
+        // If no content was processed, emit initial events first
+        if (this._currentState == State.Initial)
+        {
+            yield break;
+        }
+
+        // Emit final events
+        var finalText = this._text.ToString();
+        var itemContent = new ItemContentOutputText
+        {
+            Text = finalText,
+            Annotations = [],
+            Logprobs = []
+        };
+
+        // Emit response.output_text.done event
+        yield return new StreamingOutputTextDone
+        {
+            SequenceNumber = seq.Increment(),
+            ItemId = this._itemId,
+            OutputIndex = outputIndex,
+            ContentIndex = 0,
+            Text = finalText
+        };
+
+        yield return new StreamingContentPartDone
+        {
+            SequenceNumber = seq.Increment(),
+            ItemId = this._itemId,
+            OutputIndex = outputIndex,
+            ContentIndex = 0,
+            Part = itemContent
+        };
+
+        yield return new StreamingOutputItemDone
+        {
+            SequenceNumber = seq.Increment(),
+            OutputIndex = outputIndex,
+            Item = new ResponsesAssistantMessageItemResource
+            {
+                Id = this._itemId,
+                Status = ResponsesMessageItemResourceStatus.Completed,
+                Content = [itemContent]
+            }
+        };
+
+        this._currentState = State.Completed;
+    }
+}

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Streaming/FunctionCallEventGenerator.cs
@@ -1,0 +1,75 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Streaming;
+
+/// <summary>
+/// A generator for streaming events from function call content.
+/// </summary>
+internal sealed class FunctionCallEventGenerator(
+        IdGenerator idGenerator,
+        SequenceNumber seq,
+        int outputIndex,
+        JsonSerializerOptions jsonSerializerOptions) : StreamingEventGenerator
+{
+    private bool _isCompleted;
+
+    public override bool IsSupported(AIContent content) => content is FunctionCallContent;
+
+    public override IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content)
+    {
+        if (this._isCompleted)
+        {
+            throw new InvalidOperationException("Cannot process content after the generator has been completed.");
+        }
+
+        if (content is not FunctionCallContent functionCallContent)
+        {
+            throw new InvalidOperationException("FunctionCallEventGenerator only supports FunctionCallContent.");
+        }
+
+        var item = functionCallContent.ToFunctionToolCallItemResource(idGenerator.GenerateFunctionCallId(), jsonSerializerOptions);
+        yield return new StreamingOutputItemAdded
+        {
+            SequenceNumber = seq.Increment(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        yield return new StreamingFunctionCallArgumentsDelta
+        {
+            SequenceNumber = seq.Increment(),
+            ItemId = item.Id,
+            OutputIndex = outputIndex,
+            Delta = item.Arguments
+        };
+
+        yield return new StreamingFunctionCallArgumentsDone
+        {
+            SequenceNumber = seq.Increment(),
+            ItemId = item.Id,
+            OutputIndex = outputIndex,
+            Arguments = item.Arguments
+        };
+
+        yield return new StreamingOutputItemDone
+        {
+            SequenceNumber = seq.Increment(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        this._isCompleted = true;
+    }
+
+    public override IEnumerable<StreamingResponseEvent> Complete()
+    {
+        this._isCompleted = true;
+        return [];
+    }
+}

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Streaming/FunctionResultEventGenerator.cs
@@ -1,0 +1,57 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Streaming;
+
+/// <summary>
+/// A generator for streaming events from function result content.
+/// </summary>
+internal sealed class FunctionResultEventGenerator(
+        IdGenerator idGenerator,
+        SequenceNumber seq,
+        int outputIndex) : StreamingEventGenerator
+{
+    private bool _isCompleted;
+
+    public override bool IsSupported(AIContent content) => content is FunctionResultContent;
+
+    public override IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content)
+    {
+        if (this._isCompleted)
+        {
+            throw new InvalidOperationException("Cannot process content after the generator has been completed.");
+        }
+
+        if (content is not FunctionResultContent functionResultContent)
+        {
+            throw new InvalidOperationException("FunctionResultEventGenerator only supports FunctionResultContent.");
+        }
+
+        var item = functionResultContent.ToFunctionToolCallOutputItemResource(idGenerator.GenerateFunctionOutputId());
+        yield return new StreamingOutputItemAdded
+        {
+            SequenceNumber = seq.Increment(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        yield return new StreamingOutputItemDone
+        {
+            SequenceNumber = seq.Increment(),
+            OutputIndex = outputIndex,
+            Item = item
+        };
+
+        this._isCompleted = true;
+    }
+
+    public override IEnumerable<StreamingResponseEvent> Complete()
+    {
+        this._isCompleted = true;
+        return [];
+    }
+}

dotnet/src/Microsoft.Agents.AI.Hosting.OpenAI/Responses/Streaming/StreamingEventGenerator.cs
@@ -1,0 +1,33 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Collections.Generic;
+using Microsoft.Agents.AI.Hosting.OpenAI.Responses.Models;
+using Microsoft.Extensions.AI;
+
+namespace Microsoft.Agents.AI.Hosting.OpenAI.Responses.Streaming;
+
+/// <summary>
+/// Abstract base class for generating streaming events from <see cref="AIContent"/> instances
+/// </summary>
+internal abstract class StreamingEventGenerator
+{
+    /// <summary>
+    /// Determines if the provided content is supported by this generator.
+    /// </summary>
+    /// <param name="content">The <see cref="AIContent"/> to check.</param>
+    /// <returns>True if the content is supported, false otherwise.</returns>
+    public abstract bool IsSupported(AIContent content);
+
+    /// <summary>
+    /// Processes a single <see cref="AIContent"/> instance and yields streaming events based on the current state.
+    /// </summary>
+    /// <param name="content">The <see cref="AIContent"/> to process.</param>
+    /// <returns>An enumerable of streaming events generated from processing the content.</returns>
+    public abstract IEnumerable<StreamingResponseEvent> ProcessContent(AIContent content);
+
+    /// <summary>
+    /// Completes the event generation and emits final events.
+    /// </summary>
+    /// <returns>An enumerable of final streaming events.</returns>
+    public abstract IEnumerable<StreamingResponseEvent> Complete();
+}